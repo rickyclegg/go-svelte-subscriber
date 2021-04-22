c9c39ea9f89c801cb42fc5e166c845c48cb8e337
Riya Dattani <riyaddattani@gmail.com>
2021-04-22 16:55:46 +0100

    dolla dolla

--- svelte/src/Counter.svelte
+++ svelte/src/Counter.svelte
@@ -1,5 +1,23 @@
 <svelte:options tag="my-counter"/>
 
+<script>
+  export let counter = 0
 
+  const onClick = () => {
+    counter += 1
+  }
 
-<p>Current count: 1</p>
+  $: counter = typeof counter === 'string' ? parseInt(counter) : counter;
+</script>
+
+<button on:click={onClick}>
+    Click me!
+</button>
+
+<p>Current count: {counter}</p>
+
+<style>
+    p {
+      font-family: 'Comic Sans MS';
+    }
+</style>
